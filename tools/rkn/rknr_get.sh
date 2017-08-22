d738ba0f8de0a7e598a8096c0f861fdaa1cda110
Oleg Nemanov <lego12239@yandex.ru>
2017-08-22 12:48:34 +0300

    tools/rkn: use bash "strict" mode for rknr_get.sh

--- tools/rkn/rknr_get.sh
+++ tools/rkn/rknr_get.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
+set -euo pipefail
+IFS=$'\n\t'
+
 PID_SELF=$$
 PIDS=$(pgrep $(basename $0))
-PIDS=$(echo "$PIDS" | grep -v $PID_SELF)
+PIDS=$(echo "$PIDS" | grep -v $PID_SELF || true)
 [[ $PIDS != "" ]] && exit
 
 DIR=${0%/*}
 
 [[ $DIR != $0 ]] && cd $DIR
 
-false
-while [[ $? -ne 0 ]]; do
+while true; do
 	ERRMSG=`./rknr_get.pl -o tf_list -r req.xml -s req.xml.sig conf 2>&1`
+	[[ $? -eq 0 ]] && break
 done
 
 kill -USR1 `cat /var/run/trfl.pid`
